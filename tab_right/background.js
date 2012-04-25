e225134ff289484215f3fb8911cd0e4ece89cd36
Hirokazu Takatama <hirokazu.takatama@gmail.com>
2012-04-25 22:19:00 +0900

    first commit

---
+++ tab_right/background.js
@@ -0,0 +1,40 @@
+﻿tab_right = {
+    on: true,
+    setOn: function (on) {
+        tab_right.on = on;
+        if(tab_right.isOn()) {
+            chrome.browserAction.setBadgeText({
+                text: 'on'
+            });
+        } else {
+            chrome.browserAction.setBadgeText({
+                text: ''
+            });
+        }
+    },
+    isOn: function () {
+        return tab_right.on;
+    },
+    toggle: function () {
+        tab_right.setOn(!tab_right.isOn());
+    },
+    current_id: null,
+    current_index: null
+}
+
+chrome.browserAction.onClicked.addListener(function (){
+    tab_right.toggle();
+});
+
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+    if (tab_right.isOn()) {
+        chrome.tabs.query({}, function (tabs) {
+            setTimeout(function () {
+                chrome.tabs.move(activeInfo.tabId, {
+                    index: tabs.length
+                }, null);
+            }, 200);
+        });
+    }
+});
+tab_right.setOn(true);
